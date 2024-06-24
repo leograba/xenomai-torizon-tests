b7183ab248d7dbf7dbf20d56f9a24aa0c616afc5
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2024-06-24 15:14:14 -0300

    deploy: put device addr in a variable
    Make it easier to change the test device in the script.
    
    Signed-off-by: Leonardo Graboski Veiga <leonardo.veiga@toradex.com>

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#DEVICE="torizon-x86.local"
+DEVICE="torizon-x86"
+
 # copy test script
 printf "Updating test script\n"
-rsync -v xenomai"$1"-torizon-tests.sh torizon@torizon-x86.local:
+rsync -v xenomai"$1"-torizon-tests.sh torizon@"$DEVICE":
 
 # execute test script
 printf "Starting tests\n\n"
-ssh -t torizon@torizon-x86.local "/home/torizon/xenomai$1-torizon-tests.sh"
+ssh -t torizon@"$DEVICE" "/home/torizon/xenomai$1-torizon-tests.sh"
